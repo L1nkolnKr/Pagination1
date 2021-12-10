cb5a3620648f650ad7231407a5f484b19413098a
l1nkolnkr <mr.aleks111@gmail.com>
2021-12-10 21:21:17 +0200

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useState, useEffect } from "react";
+import {Container, Pagination, TextField, Stack, Link} from '@mui/material'
+
+const BASE_URL = 'http://hn.algolia.com/api/v1/search?'
 
 function App() {
+  const[posts, setPosts] = useState([]);
+  const[queru, seQuery] = useState('react');
+  const[page, setPage] = useState(1);
+  const[pageQty, setPageQty] = useState(0);
+
+  useEffect(() =>{
+    axios.get(BASE_URL + `query=${query}&page=${page - 1}`).then(
+      ({data}) => {
+        setPosts(data.hits)
+        setPageQty(data.nbPages)
+      }
+    )
+  }, [query, page])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <TextField
+      fullWidth
+      label="query"
+      value={query}
+      onChangeCapture={(event) => setQuery(event.target.value)}
+      />
+      <Stack spacing={2}>
+        <Pagination/>
+      </Stack>
+    </Container>
   );
 }
+
+export default App;
 
 export default App;
